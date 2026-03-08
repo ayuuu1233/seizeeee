ea011c74fb58136c0c53fdf70970eddcad0c48fe
ᴀʏᴜsʜʜ ᴘᴀʀᴋ <maharanachinmaya79@gmail.com>
2026-03-08 19:34:40 +0530

    Update guess.py

--- shivu/modules/guess.py
+++ shivu/modules/guess.py
@@ -3,7 +3,7 @@ from telegram.ext import CommandHandler, CallbackQueryHandler
 from shivu import application, user_collection
 
 # Replace OWNER_ID with the actual owner's user ID
-OWNER_ID = 6835013483
+OWNER_ID = 5158013355
 
 async def transfer(update, context):
     try:
